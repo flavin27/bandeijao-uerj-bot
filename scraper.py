8607df252cc80ef43585dc919ed3270ddfe480c2
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-05-15 13:50:19 -0300

    added proxy

--- scraper.py
+++ scraper.py
@@ -7,6 +7,15 @@ from datetime import datetime
 class Scraper:
     def __init__(self):
         self.url = "https://www.restauranteuniversitario.uerj.br/"
+        self.headers = {
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        }
 
 
     def normalize_text(self, text):
@@ -18,7 +27,7 @@ class Scraper:
         return dia_da_semana
     
     def scrape_data(self):
-        response = requests.get(self.url)
+        response = requests.get(self.url, headers=self.headers)
         content = response.content.decode()
         normalized_content = self.normalize_text(content)
 
